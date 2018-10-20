4e43d8bafbcf8d9abf6c007e33974b1e66cf3e2f
Veit Progl <er>
2018-10-20 18:01:21 +0200

    Terwrwrwfrwr

--- client/src/components/ShowPost.vue
+++ client/src/components/ShowPost.vue
@@ -12,7 +12,7 @@
             <option value="saab">Saab</option>
             <option value="opel">Opel</option>
             <option value="audi">Audi</option>
-          </select>
+          </select>    
 
           <span>{{components}}</span>
         </div>
